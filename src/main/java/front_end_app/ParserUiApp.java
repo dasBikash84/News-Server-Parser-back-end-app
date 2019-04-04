d18ecb44c53a99e4e57c14b3c94f34915fa6c7c9
Bikash Das <bikashdaseee@gmail.com>
2019-04-04 12:19:53 +0600

    JavaFx application created and relevantdependencies included. Fixes #2

---
+++ src/main/java/front_end_app/ParserUiApp.java
@@ -0,0 +1,20 @@
+package front_end_app;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ParserUiApp extends Application {
+    @Override
+    public void start(Stage window) throws Exception {
+
+        window.setTitle("News Server Parser");
+        window.setMinWidth(250);
+        window.setMinHeight(250);
+
+        setUserAgentStylesheet(STYLESHEET_CASPIAN);
+
+
+        window.show();
+    }
+}
