f72011b79c38050c42adb7028f5d01af04d46ed1
Bikash Das <bikashdaseee@gmail.com>
2019-04-08 07:07:56 +0600

    Copyright notice added to whole project. Fixes #10 and Fixes dasBikash84/NewsServer_Reloaded#1

--- src/main/java/com/dasbikash/news_server_parser/utils/ToDoUtils.java
+++ src/main/java/com/dasbikash/news_server_parser/utils/ToDoUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 www.dasbikash.org. All rights reserved.
+ * Copyright 2019 das.bikash.dev@gmail.com. All rights reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
